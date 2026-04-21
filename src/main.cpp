ac95d78381f4c65bbcb9bd55c6b7b728d8454644
Rutten, Lindy L.M.F. <lindy.rutten@student.fontys.nl>
2026-04-21 23:25:06 +0200

    Replace buttons with input based on sound sensors. Make eyes look towards sounds

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,41 @@ const int rotateRight = 3;
 const int rotateUp = 4;
 const int rotateDown = 5;
 
+const int micLeftPin = A0;
+const int micRightPin = A1;
+
 int oogPos = 90;
+int targetPos = 90;
 int upDownPos = 90;
 
 const int stap = 5;
+
+int leftOffset = 0;
+int rightOffset = 0;
+
+int noiseThreshold = 5;
+int dominanceGap = 5;
+
+int winner = 0;
+
+unsigned long lastSwitchTime = 0;
+int lockTime = 300; 
+
+int smoothSpeed = 2; 
+
+void calibrateMics() {
+  long leftSum = 0;
+  long rightSum = 0;
+
+  for (int i = 0; i < 100; i++) {
+    leftSum += analogRead(micLeftPin);
+    rightSum += analogRead(micRightPin);
+    delay(10);
+  }
+
+  leftOffset = leftSum / 100;
+  rightOffset = rightSum / 100;
+}
 
 void setup() {
   eyeServo.attach(eyeServoPin);
@@ -28,34 +59,94 @@ void setup() {
   pinMode(rotateRight, INPUT_PULLUP);
   pinMode(rotateUp, INPUT_PULLUP);
   pinMode(rotateDown, INPUT_PULLUP);
+
+  Serial.begin(9600);
+
+  calibrateMics();
 }
 
 void loop() {
+
   if (digitalRead(rotateLeft) == LOW) {
-    oogPos = oogPos - stap;
-    if (oogPos < 0) oogPos = 0;
-    eyeServo.write(oogPos);
+    targetPos -= stap;
+    if (targetPos < 0) targetPos = 0;
     delay(100);
   }
 
   if (digitalRead(rotateRight) == LOW) {
-    oogPos = oogPos + stap;
-    if (oogPos > 180) oogPos = 180;
-    eyeServo.write(oogPos);
+    targetPos += stap;
+    if (targetPos > 180) targetPos = 180;
     delay(100);
   }
 
   if (digitalRead(rotateUp) == LOW) {
-    upDownPos = upDownPos - stap;
+    upDownPos -= stap;
     if (upDownPos < 0) upDownPos = 0;
     nodServo.write(upDownPos);
     delay(100);
   }
 
   if (digitalRead(rotateDown) == LOW) {
-    upDownPos = upDownPos + stap;
+    upDownPos += stap;
     if (upDownPos > 180) upDownPos = 180;
     nodServo.write(upDownPos);
     delay(100);
   }
+
+  int leftRaw = analogRead(micLeftPin);
+  int rightRaw = analogRead(micRightPin);
+
+  int leftLevel = abs(leftRaw - leftOffset);
+  int rightLevel = abs(rightRaw - rightOffset);
+
+  leftLevel *= 1.2;
+
+  leftLevel = max(0, leftLevel - noiseThreshold);
+  rightLevel = max(0, rightLevel - noiseThreshold);
+
+  unsigned long now = millis();
+
+  if (now - lastSwitchTime > lockTime) {
+
+    if (leftLevel > rightLevel + dominanceGap && winner != -1) {
+      winner = -1;
+      lastSwitchTime = now;
+    }
+    else if (rightLevel > leftLevel + dominanceGap && winner != 1) {
+      winner = 1;
+      lastSwitchTime = now;
+    }
+  }
+
+  if (winner == -1) {
+    targetPos = 20;
+  }
+  else if (winner == 1) {
+    targetPos = 160;
+  }
+
+  if (oogPos < targetPos) {
+    oogPos += smoothSpeed;
+    if (oogPos > targetPos) oogPos = targetPos;
+    eyeServo.write(oogPos);
+  }
+  else if (oogPos > targetPos) {
+    oogPos -= smoothSpeed;
+    if (oogPos < targetPos) oogPos = targetPos;
+    eyeServo.write(oogPos);
+  }
+
+  // Debug
+  Serial.print("L: ");
+  Serial.print(leftLevel);
+  Serial.print(" R: ");
+  Serial.print(rightLevel);
+  Serial.print(" Winner: ");
+  if (winner == -1) Serial.print("Left");
+  else if (winner == 1) Serial.print("Right");
+  else Serial.print("None");
+  Serial.print(" Pos: ");
+  Serial.println(oogPos);
+
+  delay(20);
 }
